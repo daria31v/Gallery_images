ddbce33c20d2a89d1b08919ddff02e97f311a88f
Daria Vdovichenko <darya.vdovichenko86@gmail.com>
2022-11-25 22:52:43 +0100

    1

---
+++ src/index.js
@@ -0,0 +1,10 @@
+import './css/styles.css';
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
+import axios, { isCancel, AxiosError } from 'axios';
+import SimpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css";
+
+console.log(Notify)
+console.log(axios)
+console.log(SimpleLightbox)
+const DEBOUNCE_DELAY = 300;
