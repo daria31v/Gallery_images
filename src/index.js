5d3aec30d01c1bbd753fdcfdcb37dff41c614b61
Daria Vdovichenko <darya.vdovichenko86@gmail.com>
2022-11-28 00:04:33 +0100

    4

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 import './css/styles.css';
 import { Notify } from 'notiflix/build/notiflix-notify-aio';
 import axios, { isCancel, AxiosError } from 'axios';
-import SimpleLightbox from 'simplelightbox';
-import 'simplelightbox/dist/simple-lightbox.min.css';
+import SimpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css";
 
 // console.log(Notify)
 // console.log(axios)
@@ -10,76 +10,120 @@ import 'simplelightbox/dist/simple-lightbox.min.css';
 const DEBOUNCE_DELAY = 300;
 const BASE_URL = 'https://pixabay.com/api/';
 const API_KEY = '31608375-581536e59e6cd039daecb6e21';
-const q = 'cat,dog';
+
 // const image = 'photo';
 // const orientation = 'horizontal';
 const params = 'image_type=photo&orientation=horizontal&safesearch=true';
-
 const form = document.querySelector('#search-form');
 const inputText = document.querySelector('input');
 const btnSearch = document.querySelector('button');
 const btnLoadMore = document.querySelector('.load-more');
 const gallery = document.querySelector('.gallery');
-// console.log(inputText)
-// const totalHitsValue = totalHits;
+btnLoadMore.classList.add("is-hidden");
+
+let page = 1;
+let limit = 40;
+
+const totalPages = 100 / limit;
 
 form.addEventListener('submit', onSearch);
+btnLoadMore.addEventListener('click', onMoreImg);
+
+
 
 function onSearch(evt) {
   evt.preventDefault();
-  console.log(evt.currentTarget);
+  // console.log(evt.currentTarget);
   const elementsEvt = evt.currentTarget.elements;
-  console.log(elementsEvt);
+  // console.log(elementsEvt);
   const inputValue = inputText.value;
   console.log(inputValue);
 
   if (!inputValue) {
     alert('Поле пусте!');
     return;
+  } 
+  pixabayApi(inputValue).then(data => creatMarkup(data));
+}
+
+async function pixabayApi(inputValue) {
+  
+  try {
+    const resp = await fetch(`${BASE_URL}?key=${API_KEY}&q=${inputValue}&${params}&limit=40&page=${page}`);
+    // console.log(resp)
+    if (!resp.ok) {
+      throw new Error(resp.statusText);
+    }
+    return await resp.json();    
+  } catch (err) {
+    return console.error(err);
   }
-  pixabayApi(inputValue).then(data => console.log(data));
 }
 
-function pixabayApi(inputValue) {
-  console.log(inputValue);
-  return fetch(`${BASE_URL}?key=${API_KEY}&q=${q}&${params}`)
-    .then(resp => {
-      // console.log(resp)
-      if (!resp.ok) {
-        throw new Error(resp.statusText);
-      }
-      return resp.json();
-    })
-    .catch(err => console.error(err));
+function creatMarkup(data) {
+  // console.log(data)
+
+  const markup = data.hits.map(({
+    largeImageURL,
+    webformatURL,
+    tags,
+    likes,
+    views,
+    comments,
+    downloads
+  }) => {
+    return `<div class="photo-card">
+      <div class = "image-preview"
+        <a class="gallery__link" href=${largeImageURL}>
+        <img class="gallery__image"
+        src=${webformatURL}
+       alt=${tags} loading="lazy"/></a>
+       </div>
+
+  <div class="info">
+    <p class="info-item">
+      <b>Likes:${likes}</b>
+    </p>
+    <p class="info-item">
+      <b>Views:${views}</b>
+    </p>
+    <p class="info-item">
+      <b>Comments: ${comments}</b>
+    </p>
+    <p class="info-item">
+      <b>Downloads: ${downloads}</b>
+    </p>
+  </div>
+</div>`;
+  }).join('');
+  gallery.insertAdjacentHTML("beforeend", markup);
 }
 
-// function creatMarkup(items) {
-//   const markup = u.map(
-//       item =>
-//         `<div class="photo-card">
-//         <a class="gallery__link" href=${item.largeImageURL}>
-//         <img class="gallery__image"
-//         src=${item.webformatURL}
-//        alt=${item.tags} loading="lazy"/></a>
-//   <div class="info">
-//     <p class="info-item">
-//       <b>Likes:${item.likes}</b>
-//     </p>
-//     <p class="info-item">
-//       <b>Views:${item.views}</b>
-//     </p>
-//     <p class="info-item">
-//       <b>Comments: ${comments}</b>
-//     </p>
-//     <p class="info-item">
-//       <b>Downloads: ${item.downloads}</b>
-//     </p>
-//   </div>
-// </div>`).join();
-//   gallery.innerHTML = markup;
+function onMoreImg() {
+  // if (page > totalPages) {
+    // return toggleAlertPopup();
+    page += 1;
+    pixabayApi(page).then(data => console.log(data))
+//  } 
+}
+
+
+function infoNotifyTotalHits() {
+    Notify.info(`Hooray! We found ${totalHits} images.`);
+}
+
+function errorNotify() {
+    Notify.failure("Oops, there is no country with that name.");
+}
+
+function endNotify() {
+  Notify.info("We're sorry, but you've reached the end of search results.");
+}
+// function toggleAlertPopup() {
+//   if()
 // }
 
-// new SimpleLightbox('.gallery a', {
+// new SimpleLightbox('.image-preview a', {
 //   captionsData: 'alt',
 //   captionDelay: 250,
 // });
