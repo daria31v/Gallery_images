27e1f87056fd9742b0bbb7949f2faf4e5169b66d
Daria Vdovichenko <darya.vdovichenko86@gmail.com>
2022-11-28 20:44:10 +0100

    5

--- src/index.js
+++ src/index.js
@@ -4,66 +4,55 @@ import axios, { isCancel, AxiosError } from 'axios';
 import SimpleLightbox from "simplelightbox";
 import "simplelightbox/dist/simple-lightbox.min.css";
 
+import NewsApiService from './new-api-service';
+
+
 // console.log(Notify)
 // console.log(axios)
 // console.log(SimpleLightbox)
-const DEBOUNCE_DELAY = 300;
 const BASE_URL = 'https://pixabay.com/api/';
 const API_KEY = '31608375-581536e59e6cd039daecb6e21';
-
-// const image = 'photo';
-// const orientation = 'horizontal';
 const params = 'image_type=photo&orientation=horizontal&safesearch=true';
+
 const form = document.querySelector('#search-form');
 const inputText = document.querySelector('input');
 const btnSearch = document.querySelector('button');
 const btnLoadMore = document.querySelector('.load-more');
 const gallery = document.querySelector('.gallery');
+
 btnLoadMore.classList.add("is-hidden");
 
-let page = 1;
-let limit = 40;
-
-const totalPages = 100 / limit;
+const newsApiService = new NewsApiService();
 
 form.addEventListener('submit', onSearch);
 btnLoadMore.addEventListener('click', onMoreImg);
 
-
-
 function onSearch(evt) {
   evt.preventDefault();
-  // console.log(evt.currentTarget);
-  const elementsEvt = evt.currentTarget.elements;
-  // console.log(elementsEvt);
-  const inputValue = inputText.value;
-  console.log(inputValue);
+  newsApiService.query = evt.currentTarget.elements.searchQuery.value;
 
-  if (!inputValue) {
-    alert('Поле пусте!');
-    return;
-  } 
-  pixabayApi(inputValue).then(data => creatMarkup(data));
-}
-
-async function pixabayApi(inputValue) {
-  
-  try {
-    const resp = await fetch(`${BASE_URL}?key=${API_KEY}&q=${inputValue}&${params}&limit=40&page=${page}`);
-    // console.log(resp)
-    if (!resp.ok) {
-      throw new Error(resp.statusText);
-    }
-    return await resp.json();    
-  } catch (err) {
-    return console.error(err);
+  if (newsApiService.query === '') {
+    return Notify.info('Please enter the query parameters.')
+    
   }
+  
+  newsApiService.resetPage();
+  newsApiService.fetchImage().then(hits => {
+    clearGalleryMarkup();
+    creatMarkup(hits);
+    btnLoadMore.classList.remove("is-hidden");
+     })
+  
 }
-
-function creatMarkup(data) {
-  // console.log(data)
-
-  const markup = data.hits.map(({
+function onMoreImg() {
+  newsApiService.fetchImage().then(creatMarkup)
+}
+function clearGalleryMarkup() {
+gallery.innerHTML = '';
+}
+function creatMarkup(hits) {
+  console.log(hits)
+  const markup = hits.map(({
     largeImageURL,
     webformatURL,
     tags,
@@ -74,10 +63,8 @@ function creatMarkup(data) {
   }) => {
     return `<div class="photo-card">
       <div class = "image-preview"
-        <a class="gallery__link" href=${largeImageURL}>
-        <img class="gallery__image"
-        src=${webformatURL}
-       alt=${tags} loading="lazy"/></a>
+        <a href=${largeImageURL} class="gallery__link">
+        <img src=${webformatURL} alt=${tags} class="gallery__image" loading="lazy"/></a>
        </div>
 
   <div class="info">
@@ -96,34 +83,16 @@ function creatMarkup(data) {
   </div>
 </div>`;
   }).join('');
-  gallery.insertAdjacentHTML("beforeend", markup);
+  gallery.insertAdjacentHTML('beforeend', markup);
+
+
+
+
+
+  // new SimpleLightbox('.photo-card a', {
+  //   captionsData: 'alt',
+  //   captionDelay: 250
+  // });
+  
+  // console.log(SimpleLightbox)
 }
-
-function onMoreImg() {
-  // if (page > totalPages) {
-    // return toggleAlertPopup();
-    page += 1;
-    pixabayApi(page).then(data => console.log(data))
-//  } 
-}
-
-
-function infoNotifyTotalHits() {
-    Notify.info(`Hooray! We found ${totalHits} images.`);
-}
-
-function errorNotify() {
-    Notify.failure("Oops, there is no country with that name.");
-}
-
-function endNotify() {
-  Notify.info("We're sorry, but you've reached the end of search results.");
-}
-// function toggleAlertPopup() {
-//   if()
-// }
-
-// new SimpleLightbox('.image-preview a', {
-//   captionsData: 'alt',
-//   captionDelay: 250,
-// });
---
+++ src/new-api-service.js
@@ -0,0 +1,50 @@
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
+
+export default class NewsApiService{
+    constructor() {
+        this.searchQuery = '';
+        this.page = 1;
+
+     }
+    async fetchImage() {
+        try {
+            const url = `https://pixabay.com/api/?key=31608375-581536e59e6cd039daecb6e21&q=${this.searchQuery}&language=en&per_page=40&page=${this.page}`;
+            
+            const resp = await fetch(url);
+            if (!resp.ok) {
+                throw new Error(resp.statusText);
+            }
+
+            const data = await resp.json();
+            console.log(data);
+            if (data.total === 0) {
+                Notify.failure("Sorry, there are no images matching your search query. Please try again.");
+                btnLoadMore.classList.add("is-hidden");
+                
+            } else if (data.hits.length === 0) {
+                Notify.warning("We're sorry, but you've reached the end of search results.");
+
+            } else if(this.page === 1){
+                Notify.info(`Hooray! We found all ${data.total} and now we show you just ${data.totalHits} images.`);
+            }
+               
+            this.page += 1;
+            return data.hits;
+
+        } catch (err) {
+            return console.log(err)
+        }
+    }
+    
+    resetPage() {
+        this.page = 1;
+    }
+
+    get query() {
+        return this.searchQuery;
+    }
+    
+    set query(newQuery) {
+        this.searchQuery = newQuery;
+    }
+}
